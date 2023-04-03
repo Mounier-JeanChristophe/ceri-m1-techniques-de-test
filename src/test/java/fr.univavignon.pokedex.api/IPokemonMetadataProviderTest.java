33a163f34cead878014cdf1e6f3acdd44da17a86
Mounier-JeanChristophe <jean-christophe.mounier@alumni.univ-avignon.fr>
2023-04-03 15:29:58 +0200

    Tentative de résolution de jacoco

--- src/test/java/fr.univavignon.pokedex.api/IPokemonMetadataProviderTest.java
+++ src/test/java/fr.univavignon.pokedex.api/IPokemonMetadataProviderTest.java
@@ -8,16 +8,16 @@ import static org.junit.Assert.assertEquals;
 public class IPokemonMetadataProviderTest {
 
     private IPokemonMetadataProvider pkmDataProvider = Mockito.mock(IPokemonMetadataProvider.class);
-    private PokemonMetadata pkmMetadata = new PokemonMetadata(1,"Grokachu", 2, 2, 2);
+    private PokemonMetadata pkmMetadata = new PokemonMetadata(0,"Bulbizarre", 126, 126, 90);
 
     @Before
     public void setUp() throws PokedexException {
-        Mockito.when(pkmDataProvider.getPokemonMetadata(1)).thenReturn(pkmMetadata);
+        Mockito.when(pkmDataProvider.getPokemonMetadata(0)).thenReturn(pkmMetadata);
     }
 
     @Test
     public void getPokemonMetadataTest() throws PokedexException {
-        assertEquals(pkmMetadata, pkmDataProvider.getPokemonMetadata(1));
+        assertEquals(pkmMetadata, pkmDataProvider.getPokemonMetadata(0));
     }
 
 }
